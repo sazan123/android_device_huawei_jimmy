fee70d03d3f5533725f615aff41538a3b124ea08
sazan123 <focking@mail.ru>
2018-03-02 14:45:17 +0200

    clean tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,9 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# config.mk
-#
-# Product-specific compile-time definitions.
 #
 
 LOCAL_PATH := device/huawei/huawei6750_66_n
--- full_huawei6750_66_n.mk
+++ full_huawei6750_66_n.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2017 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := device/huawei/huawei6750_66_n
-
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 
 PRODUCT_BRAND := HUAWEI
 PRODUCT_NAME := full_huawei6750_66_n
