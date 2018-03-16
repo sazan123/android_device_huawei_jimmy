1770816044b30747686e8c320224ee38275905e8
sazan123 <focking@mail.ru>
2018-03-16 12:21:31 +0200

    Upd. tree. Remove unused files.

--- recovery/root/enableswap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo lz4 > /sys/block/zram0/comp_algorithm
-echo 3 > /sys/block/zram0/max_comp_streams
-echo 0 > /sys/block/zram0/disksize
-/system/bin/mkswap /dev/block/zram0
-/system/bin/swapon /dev/block/zram0
